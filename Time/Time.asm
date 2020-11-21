f4517f5b8187275576c9e50010981217371031db
rohitchadichal-99 <54311681+rohitchadichal-99@users.noreply.github.com>
2020-11-21 10:06:12 +0530

    Add files via upload

---
+++ Time/Time.asm
@@ -0,0 +1,29 @@
+.model small
+.code
+mov ah,2ch
+int 21h
+mov al,ch
+aam
+mov bx,ax
+call disp
+mov dl,':'
+mov ah,02h
+int 21h
+mov al,dh
+aam
+mov bx,ax
+call disp
+mov ah,4ch
+int 21h
+disp proc near
+mov dl,bh
+add dl,30h
+mov ah,02h
+int 21h
+mov dl,bl
+add dl,30h
+mov ah,02h
+int 21h
+ret
+disp endp
+end
